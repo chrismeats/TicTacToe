4b3142aab80b5ed61435bf3a28919c4cc07d8cf6
Chris Meats <cmeats@etccomputerland.com>
2014-07-24 14:05:06 -0500

    first commit

---
+++ TicTacToe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TicTacToe
+//
+//  Created by ETC ComputerLand on 7/24/14.
+//  Copyright (c) 2014 cmeats. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
